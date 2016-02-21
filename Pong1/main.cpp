359779c2d1a676a2c73bec9f890c0edc3e0ebea0
Koissi Adjorlolo <imapersonman@koissis-mac-pro.local>
2016-02-20 17:46:42 -0800

    Initial Commit

---
+++ Pong1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pong1
+//
+//  Created by Koissi Adjorlolo on 2/20/16.
+//  Copyright © 2016 centuryapps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
